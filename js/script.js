e87ec1b3e8f9003513f9aa6c6873b266cb0bb350
Karel <kareltrisnanto26@gmail.com>
2023-03-21 17:22:09 +0700

    Add Sticky Button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Humburger Button Menu
+
 const humburger = document.querySelector(".humburger");
 const navBar = document.querySelector("nav");
 const navList = document.querySelectorAll(".nav-list");
@@ -23,3 +25,25 @@ humburger.addEventListener('click', () => {
         })
     })
 })
+
+// Sticky Back To Top
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
