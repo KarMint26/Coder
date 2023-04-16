bca0379ac9b79ae7c4d971257fedfbf6bbe425c8
Karel <kareltrisnanto26@gmail.com>
2023-04-16 14:28:53 +0700

    Create Loader

--- js/script.js
+++ js/script.js
@@ -47,3 +47,10 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Loader Animation
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.querySelector(".loader-container").style.opacity = "0";
+    }, 1000);
+});
